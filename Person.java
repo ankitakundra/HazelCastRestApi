22560aa15859991ef7feac0209b07314403802bf
ankitakundra <33183541+ankitakundra@users.noreply.github.com>
2017-11-21 01:06:57 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+package com.hazelcast.rest.RestClientIntegration;
+import java.io.Serializable;
+
+/**
+ * Simple Person class
+ */
+@SuppressWarnings("unused")
+public class Person implements Serializable {
+
+    private String name;
+
+    Person(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Person {name='" + name + "'}";
+    }
+}
